4c635d50853be29c3ea4f1903216bf025ad766e2
HSGVitor <135385429+HSGVitor@users.noreply.github.com>
2024-11-06 10:21:33 -0300

    Update script3.js

--- Z/script3.js
+++ Z/script3.js
@@ -3,7 +3,7 @@ function verificarSenha() {
     const senhaCorreta = "Um dia, talvez"; // Frase correta para validação
 
     if (entrada === senhaCorreta) {
-        window.location.href = "pagina2.html"; // Redireciona para uma nova página
+        window.location.href = "pagina4.html"; // Redireciona para uma nova página
     } else {
         alert("Senha incorreta");
         document.getElementById("entrada-pesquisa").value = ""; // Limpa o campo de entrada
