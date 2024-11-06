17a1cb0137384d5de97df04c0fe14c4423a33784
HSGVitor <135385429+HSGVitor@users.noreply.github.com>
2024-11-06 10:15:21 -0300

    Codigos ja feitos

---
+++ Z/script3.js
@@ -0,0 +1,11 @@
+function verificarSenha() {
+    const entrada = document.getElementById("entrada-pesquisa").value;
+    const senhaCorreta = "Um dia, talvez"; // Frase correta para validação
+
+    if (entrada === senhaCorreta) {
+        window.location.href = "pagina2.html"; // Redireciona para uma nova página
+    } else {
+        alert("Senha incorreta");
+        document.getElementById("entrada-pesquisa").value = ""; // Limpa o campo de entrada
+    }
+}
