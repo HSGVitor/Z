a0c2814bfe0abf510bc09b5c6dc3746316beb0de
HSGVitor <135385429+HSGVitor@users.noreply.github.com>
2024-11-18 10:34:25 -0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
-    <title>5</title>
-</head>
-<body>
-    <div class="card-container">
-        <div class="container-texto">
-            <h1 style="color: black; line-height: 0;">Eu estava</h1>
-            <h1> escondido</h1>
-            <p style="color: black; font-weight: bold; font-size: 40px;">^^^^^^^^</p>
-            
-        <div class="area-pesquisa">
-            <input type="text" class="entrada-pesquisa" id="entrada-pesquisa" placeholder="Resposta...">
-            <button class="botao-pesquisa" onclick="verificarSenha()">Enviar</button>
-        </div>
-    </div>
+function verificarSenha() {
+    const entrada = document.getElementById("entrada-pesquisa").value;
+    const senhaCorreta = "Eu estava escondido"; // Frase correta para validação
 
-    <script src="script5.js"></script>
-</body>
-</html>
+    if (entrada === senhaCorreta) {
+        window.location.href = "pagina6.html"; // Redireciona para uma nova página
+    } else {
+        alert("Senha incorreta");
+        document.getElementById("entrada-pesquisa").value = ""; // Limpa o campo de entrada
+    }
+}
