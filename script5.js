d6e1840f503cc347fee09f830ac043151a8f2af1
HSGVitor <135385429+HSGVitor@users.noreply.github.com>
2024-11-18 10:26:03 -0300

    Create script5.js

---
+++ script5.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Barra de Pesquisa</title>
+</head>
+<body>
+    <div class="card-container">
+        <div class="container-texto">
+            <h1 style="color: black; line-height: 0;">Eu estava</h1>
+            <h1> escondido</h1>
+            <p style="color: black; font-weight: bold; font-size: 40px;">^^^^^^^^</p>
+            
+        <div class="area-pesquisa">
+            <input type="text" class="entrada-pesquisa" id="entrada-pesquisa" placeholder="Resposta...">
+            <button class="botao-pesquisa" onclick="verificarSenha()">Enviar</button>
+        </div>
+    </div>
+
+    <script src="script5.js"></script>
+</body>
+</html>
